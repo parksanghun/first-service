566b2efb881967e90fb60efc34fb934379e9b256
sanghun <sanghun1984@gmail.com>
2022-03-09 20:02:07 +0900

    init project

---
+++ src/main/java/kr/co/sanghun/study/firstservice/FirstServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.co.sanghun.study.firstservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstServiceApplication.class, args);
+    }
+
+}
