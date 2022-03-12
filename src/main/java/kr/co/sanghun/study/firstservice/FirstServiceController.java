551bac54974c65b0609bd465e48d9dcbbfd2050c
sanghun <sanghun1984@gmail.com>
2022-03-12 14:43:34 +0900

    add sources

--- src/main/java/kr/co/sanghun/study/firstservice/FirstServiceController.java
+++ src/main/java/kr/co/sanghun/study/firstservice/FirstServiceController.java
@@ -1,15 +1,21 @@
 package kr.co.sanghun.study.firstservice;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RequestMapping("/first-service")
+@RequiredArgsConstructor
 @RestController
 public class FirstServiceController {
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -20,5 +26,11 @@ public class FirstServiceController {
     public String message(@RequestHeader("first-request") String header) {
         log.info(header);
         return "Hello World in First Service";
+    }
+
+    @GetMapping("/check")
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format("This message is from first service %s", env.getProperty("local.server.port"));
     }
 }
